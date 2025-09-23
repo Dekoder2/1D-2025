3700915982d79d4da82af0789d7fb14f956edbcb
Sala213 <Sala213@ezse.pl>
2025-09-23 09:51:22 +0200

    1

--- Programowanie/FirstConsoleApplication/FirstConsoleApplication.cpp
+++ Programowanie/FirstConsoleApplication/FirstConsoleApplication.cpp
@@ -1,20 +1,39 @@
-ï»¿// FirstConsoleApplication.cpp : This file contains the 'main' function. Program execution begins and ends there.
+// FirstConsoleApplication.cpp : This file contains the 'main' function. Program execution begins and ends there.
 //
 
-#include <iostream>
+//to jest komentarz
+
+/*
+
+Algorytm - zbiór instrukcji ktory po wykananiu rozwi¹zuje postawiony przed nim problem.
+
+*/
+
+//dodanie pliku iostream.h czyli skorzystanie z biblioteki
+#include <iostream> 
 
 int main()
 {
-    std::cout << "Hello World!\n";
+	std::cout << "Hello World!\n"; //sta³a ³añcuchowa; sta³a tekstowa
+	std::cout << "Hel\tlo World!\n";
+	std::cout << 'h'; //sta³a znakowa
+	std::cout << '\n'; //sta³a znakowa
+	std::cout << 59; //sta³a ca³kowitoliczbowa
+	std::cout << '\n'; 
+	std::cout << -2.5; //sta³a rzeczywista
+	std::cout << "Pierwsza liczba " << 254 << " kolejny tekst";
+
 }
 
-// Run program: Ctrl + F5 or Debug > Start Without Debugging menu
-// Debug program: F5 or Debug > Start Debugging menu
+/*
+Run program : Ctrl + F5 or Debug > Start Without Debugging menu
+ Debug program: F5 or Debug > Start Debugging menu
 
-// Tips for Getting Started: 
-//   1. Use the Solution Explorer window to add/manage files
-//   2. Use the Team Explorer window to connect to source control
-//   3. Use the Output window to see build output and other messages
-//   4. Use the Error List window to view errors
-//   5. Go to Project > Add New Item to create new code files, or Project > Add Existing Item to add existing code files to the project
-//  6. In the future, to open this project again, go to File > Open > Project and select the .sln file
+ Tips for Getting Started:
+   1. Use the Solution Explorer window to add/manage files
+   2. Use the Team Explorer window to connect to source control
+   3. Use the Output window to see build output and other messages
+   4. Use the Error List window to view errors
+  5. Go to Project > Add New Item to create new code files, or Project > Add Existing Item to add existing code files to the project
+   6. In the future, to open this project again, go to File > Open > Project and select the .sln file
+*/
